3bca7675311f4fe19c8799d4eb5bc9035e09a2ad
developers-nodejs <maliktow012@gmail.com>
2024-02-19 00:26:06 +0530

    creating the theme infrastructure

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurentsScreen } from "./src/features/restaurents/screens/restaurents.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurentsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurentsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
