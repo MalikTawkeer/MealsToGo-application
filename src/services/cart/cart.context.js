6fef08e39483c9da00fb4125ab53f83510b1158c
developers-nodejs <maliktow012@gmail.com>
2024-03-05 00:02:16 +0530

    added save cart into local storage

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AuthenticationContext } from "../authentication/authentication.context";
 
@@ -10,6 +11,44 @@ export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [sum, setSum] = useState(0);
 
+  const addRst = async (rst) => {
+    if (restaurant) {
+      await AsyncStorage.setItem(
+        `${user.uid}-cart-restaurant`,
+        JSON.stringify(rst)
+      );
+      console.log("RST_Done");
+    }
+  };
+
+  const addCrt = async () => {
+    if (cart) {
+      await AsyncStorage.setItem(`${user.uid}-cart`, JSON.stringify(cart));
+      console.log("CRT_Done");
+    }
+  };
+
+  const retriveRstInfo = async () => {
+    const rst = await AsyncStorage.getItem(`${user.uid}-cart-restaurant`);
+    if (rst.length > 0) {
+      setRestaurant(JSON.parse(rst));
+      console.log(rst);
+    }
+  };
+  const retriveCartInfo = async () => {
+    const crt = await AsyncStorage.getItem(`${user.uid}-cart`);
+    if (crt.length > 0) {
+      setCart(JSON.parse(crt));
+    }
+  };
+
+  //retrive cart info from storage
+  useEffect(() => {
+    retriveRstInfo();
+    retriveCartInfo();
+  }, []);
+
+  
   useEffect(() => {
     if (!cart.length) {
       setSum(0);
@@ -25,6 +64,8 @@ export const CartContextProvider = ({ children }) => {
     if (!restaurant || restaurant.placeId !== rst.placeId) {
       setRestaurant(rst);
       setCart([item]);
+      addRst(rst); // to async storage
+      addCrt(cart); // to async storage
     } else {
       setCart([...cart, item]);
     }
@@ -42,7 +83,7 @@ export const CartContextProvider = ({ children }) => {
         clearCart: clear,
         restaurant,
         cart,
-        sum
+        sum,
       }}
     >
       {children}
